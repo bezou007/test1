8e53f16dfb1ace61e95623c72593448b43142079
bezouAndria <bezou007@gmail.com>
2023-11-08 08:24:50 +0300

    jekyll

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    basePath: '/test1', // Remplacez 'nom-de-votre-repo' par le nom de votre dépôt GitHub
+  }
