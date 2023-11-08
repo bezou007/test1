6d1effca7ac90fd652b72a6c4fd986f96e02f60d
bezouAndria <bezou007@gmail.com>
2023-11-08 15:12:59 +0300

    version

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output:"export"
+}
 
-module.exports = {
-    basePath: '/test1', // Remplacez 'nom-de-votre-repo' par le nom de votre dépôt GitHub
-  }
+module.exports = nextConfig
--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -1,17 +1,14 @@
 // components/Sidebar.tsx
 'use client'
 import Image from 'next/image';
-import React from 'react';
-import { Typography} from '@mui/material';
+import React, { useState }  from 'react';
+import { Card, CardContent, Container, Typography, CardMedia, AppBar, Toolbar } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import AddIcon from '@mui/icons-material/Add';
 import '../Styles/Sidebar.css';
 import { styled } from '@mui/material/styles';
-import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
 import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
-import MuiAccordionSummary, {
-  AccordionSummaryProps,
-} from '@mui/material/AccordionSummary';
+import MuiAccordionSummary, { AccordionSummaryProps,} from '@mui/material/AccordionSummary';
 import MuiAccordionDetails from '@mui/material/AccordionDetails';
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import DoneIcon from '@mui/icons-material/Done';
@@ -20,7 +17,8 @@ import SearchIcon from '@mui/icons-material/Search';
 import BadgeIcon from '@mui/icons-material/Badge';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation';
+
 
 const Accordion = styled((props: AccordionProps) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
@@ -46,12 +44,95 @@ const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
 
 
 const Sidebar: React.FC = () => {
-  const pathname = usePathname()
+
+  const menuItems = [
+    {
+      title: 'Planning',
+      image: '/Module_Planning.svg', // Replace with your image path
+      marginLeft: '30px',
+      fontweight:400
+    },
+    {
+      title: 'Absences',
+      image: '/Module_Absences.svg', // Replace with your image path
+      marginLeft: '30px',
+      border: '2px solid #0090F5', // Ajoutez une bordure transparente
+      textColor: '#0090F5',
+      fontweight:500 // Définissez la couleur du texte sur noir
+    },
+    {
+      title: 'Heures',
+      image: '/Module_Absences.svg', // Replace with your image path
+      marginLeft: '40px',
+      fontweight:400
+    },
+    {
+      title: 'Note de frais',
+      image: '/Module_NotedeFrais.svg', // Replace with your image path  
+      marginLeft: '20px',
+      fontweight:400
+    },
+    {
+      title: 'Présence',
+      image: '/Module_Présence.svg', // Replace with your image path  
+      marginLeft: '30px',
+      fontweight:400
+    },
+    {
+      title: 'Compétence',
+      image: '/Module_Compétences.svg', // Replace with your image path 
+       marginLeft: '20px',
+       fontweight:400
+    },
+    {
+      title: 'Salaire et paie',
+      image: '/Module_SalaireetPaie.svg', 
+       marginLeft: '10px',
+       fontweight:400
+    },
+    {
+      title: 'Entretiens',
+      image: '/Module_Entretiens.svg', // Replace with your image path 
+       marginLeft: '30px',  fontweight:400
+
+    },
+    {
+      title: 'Matériels',
+      image: '/Module_Matériels.svg', // Replace with your image path  
+      marginLeft: '30px',
+      fontweight:400
+    },
+    {
+      title: 'Documents',
+      image: '/Mdule_Documents.svg', // Replace with your image path  
+      marginLeft: '25px',
+      fontweight:400
+    },
+    {
+      title: 'RH',
+      image: '/Module_RH.svg', // Replace with your image path  
+      marginLeft: '55px',
+      fontweight:400
+    },
+    // Add more menu items
+  ];
+
+  const pathname = usePathname();
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    if (isMenuOpen) {
+      setIsMenuOpen(false);
+    } else {
+      setIsMenuOpen(true);
+    }
+  };
   return (
-  
-    <div className="sidebar">
+    <div className="sidebar-container">
+    <div className={`sidebar ${isMenuOpen ? 'menu-open' : ''}`}>
       <div className='burg'>
-      <button  className='buttonburg' >
+      <button  className='buttonburg'  onClick={toggleMenu}>
         <Image
         src="/Burger button.svg"
         alt="Description de l'image"
@@ -63,7 +144,7 @@ const Sidebar: React.FC = () => {
         <div  className='Acceuil' >
           <HomeIcon  className='Homeicon'/>
           <Typography className='sous11' variant="h4" >
-            <p>Acceuil</p> 
+            <p>Accueil</p> 
           </Typography>
         </div>
 
@@ -152,8 +233,40 @@ const Sidebar: React.FC = () => {
           />
 
         </div>
-        
+       
     </div>
+     <div className={`menu ${isMenuOpen ? 'menu-open' : ''}`}>
+     <div className="menu-content">
+     <AppBar position="static" style={{background:"white",boxShadow:'none',borderRadius:'40px'}}>
+     <Image
+          style={{marginTop:"118px",marginLeft:"224px"}}
+          src="/Logo.svg"
+          alt="Description de l'image"
+          width={350} // Largeur de l'image en pixels
+          height={72.5}// Hauteur de l'image en pixels
+          />
+      </AppBar>
+      <Container style={{marginTop:'109px',paddingBottom:'280px'}}>
+        <div style={{ display: 'flex', flexWrap: 'wrap',marginLeft:'7px' }}>
+          {menuItems.map((item, index) => (
+            <Card key={index} style={{ width:'170px', marginTop:'6px', marginLeft:'6px', height:"170",borderRadius:"10px",background:"#FFFFFF", border: item.border }}>
+              <CardMedia style={{width:'89px',height:'83px',marginLeft:'40px',marginTop:'10px'}}
+                component="img"
+                alt={item.title}
+                image={item.image}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" className='typo1' component="div" style={{fontFamily:'Poppins',fontSize:'15px',fontWeight: item.fontweight,color: item.textColor,marginLeft: item.marginLeft, }}>
+                  {item.title}
+                </Typography>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </Container>
+     </div>
+   </div>
+   </div>
   );
 };
 
